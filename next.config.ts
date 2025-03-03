d0c6e8c177a74b6ca6d97d8939f4685e198763b3
Eduardo Nakamura <50833822+eduardo-nakamura@users.noreply.github.com>
2025-03-03 16:21:26 -0300

    next config correcao

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@
 const nextConfig = {
   output: 'export',
   basePath: '/veterinario-next',
+  assetPrefix: '/veterinario-next',
   images: {
     unoptimized: true, // Required for static exports
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/veterinario-next/' : '', //Important for github pages subdirectories
+  //assetPrefix: process.env.NODE_ENV === 'production' ? '/veterinario-next/' : '', 
 };
 
 module.exports = nextConfig;
